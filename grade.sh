72ca99ebd9396c649ec7e7558e2d0cd8f46bf97f
noahdj37 <noahdj37@gmail.com>
2022-02-10 12:19:18 -0800

    Update grade.sh
    -rf

--- grade.sh
+++ grade.sh
@@ -3,5 +3,5 @@ git clone $1
 if test -f "./markdown-parse/MarkdownParse.java"; then
     cp ./markdown-parse/MarkdownParse.java .
     make grade
-    rm rf ./markdown-parse
+    rm -rf ./markdown-parse
 fi
